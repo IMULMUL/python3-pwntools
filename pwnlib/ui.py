IMULMUL/python3-pwntools

commit 13ef8db60c930f725659e5e52b510388556c26d8

PEP 8 compliance
Using autopep8 --in-place --aggressive --aggressive --max-line-length 90 -r .

pwnlib/ui.py
@@ -5,7 +5,8 @@ from .log import getLogger
 
 log = getLogger(__name__)
 
-def yesno(prompt, default = None):
+
+def yesno(prompt, default=None):
     """Presents the user with prompt (typically in the form of question) which
     the user must answer yes or no.
 
@@ -24,18 +25,18 @@ def yesno(prompt, default = None):
         term.output(' [?] %s [' % prompt)
         yesfocus, yes = term.text.bold('Yes'), 'yes'
         nofocus, no = term.text.bold('No'), 'no'
-        hy = term.output(yesfocus if default == True else yes)
+        hy = term.output(yesfocus if default else yes)
         term.output('/')
         hn = term.output(nofocus if default == False else no)
         term.output(']\n')
         cur = default
         while True:
             k = term.key.get()
-            if   k in ('y', 'Y', '<left>') and cur != True:
+            if k in ('y', 'Y', '<left>') and not cur:
                 cur = True
                 hy.update(yesfocus)
                 hn.update(no)
-            elif k in ('n', 'N', '<right>') and cur != False:
+            elif k in ('n', 'N', '<right>') and cur:
                 cur = False
                 hy.update(yes)
                 hn.update(nofocus)
@@ -44,20 +45,21 @@ def yesno(prompt, default = None):
                     return cur
     else:
         prompt = ' [?] %s [%s/%s] ' % (prompt,
-                                       'Yes' if default == True else 'yes',
-                                       'No' if default == False else 'no',
+                                       'Yes' if default else 'yes',
+                                       'No' if not default else 'no',
                                        )
         while True:
             opt = input(prompt).lower()
-            if opt == '' and default != None:
+            if opt == '' and default is not None:
                 return default
-            elif opt in ('y','yes'):
+            elif opt in ('y', 'yes'):
                 return True
             elif opt in ('n', 'no'):
                 return False
             print('Please answer yes or no')
 
-def options(prompt, opts, default = None):
+
+def options(prompt, opts, default=None):
     """Presents the user with a prompt (typically in the
     form of a question) and a number of options.
 
@@ -81,10 +83,10 @@ def options(prompt, opts, default = None):
         arrow = term.text.bold_green('    => ')
         cur = default
         for i, opt in enumerate(opts):
-            h = term.output(arrow if i == cur else space, frozen = False)
+            h = term.output(arrow if i == cur else space, frozen=False)
             num = numfmt % (i + 1)
             term.output(num)
-            term.output(opt + '\n', indent = len(num) + len(space))
+            term.output(opt + '\n', indent=len(num) + len(space))
             hs.append(h)
         ds = ''
         prev = 0
@@ -92,7 +94,7 @@ def options(prompt, opts, default = None):
             prev = cur
             was_digit = False
             k = term.key.get()
-            if   k == '<up>':
+            if k == '<up>':
                 if cur is None:
                     cur = 0
                 else:
@@ -128,7 +130,7 @@ def options(prompt, opts, default = None):
                 else:
                     hs[cur].update(arrow)
     else:
-        linefmt =       '       %' + str(len(str(len(opts)))) + 'd) %s'
+        linefmt = '       %' + str(len(str(len(opts)))) + 'd) %s'
         while True:
             print(' [?] ' + prompt)
             for i, opt in enumerate(opts):
@@ -143,10 +145,11 @@ def options(prompt, opts, default = None):
             if x >= 1 and x <= len(opts):
                 return x
 
-def pause(n = None):
+
+def pause(n=None):
     """Waits for either user input or a specific number of seconds."""
 
-    if n == None:
+    if n is None:
         if term.term_mode:
             log.info('Paused (press any to continue)')
             term.getkey()
@@ -161,6 +164,7 @@ def pause(n = None):
             l.success()
     else:
         raise ValueError('pause(): n must be a number or None')
+
 
 def more(text):
     """more(text)
@@ -177,7 +181,7 @@ def more(text):
     """
     if term.term_mode:
         lines = text.split('\n')
-        h = term.output(term.text.reverse('(more)'), float = True, frozen = False)
+        h = term.output(term.text.reverse('(more)'), float=True, frozen=False)
         step = term.height - 1
         for i in range(0, len(lines), step):
             for l in lines[i:i + step]:

commit 9045659e287caa7e2bfc680204bbb5f5ce50a0cb

Fix regression caused by commit 13ef8db

pwnlib/ui.py
@@ -25,18 +25,18 @@ def yesno(prompt, default=None):
         term.output(' [?] %s [' % prompt)
         yesfocus, yes = term.text.bold('Yes'), 'yes'
         nofocus, no = term.text.bold('No'), 'no'
-        hy = term.output(yesfocus if default else yes)
+        hy = term.output(yesfocus if default is True else yes)
         term.output('/')
-        hn = term.output(nofocus if default == False else no)
+        hn = term.output(nofocus if default is False else no)
         term.output(']\n')
         cur = default
         while True:
             k = term.key.get()
-            if k in ('y', 'Y', '<left>') and not cur:
+            if k in ('y', 'Y', '<left>') and cur is not True:
                 cur = True
                 hy.update(yesfocus)
                 hn.update(no)
-            elif k in ('n', 'N', '<right>') and cur:
+            elif k in ('n', 'N', '<right>') and cur is not False:
                 cur = False
                 hy.update(yes)
                 hn.update(nofocus)
@@ -45,8 +45,8 @@ def yesno(prompt, default=None):
                     return cur
     else:
         prompt = ' [?] %s [%s/%s] ' % (prompt,
-                                       'Yes' if default else 'yes',
-                                       'No' if not default else 'no',
+                                       'Yes' if default is True else 'yes',
+                                       'No' if default is False else 'no',
                                        )
         while True:
             opt = input(prompt).lower()