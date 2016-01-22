1f4d264461222b625ac35b7b008220126e14ed01
Maxime Arthaud <maxime@arthaud.me>
2016-01-22 16:32:37 +0100

    Fix documentation generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,9 @@ import sys
 import os
 import subprocess
 
+# Disable terminal handling in pwnlib
+os.environ['PWNLIB_NOTERM'] = '1'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
