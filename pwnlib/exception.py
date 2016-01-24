98ecb05ece0c5ae3bd7011a2c6454b77b7132e54
Maxime Arthaud <maxime@arthaud.me>
2016-01-24 16:12:08 +0100

    Fix: BaseException.message does not exist anymore

--- pwnlib/exception.py
+++ pwnlib/exception.py
@@ -9,17 +9,19 @@ class PwnlibException(Exception):
     Pwnlib functions that encounters unrecoverable errors should call the
     :func:`pwnlib.log.error` function instead of throwing this exception directly.'''
 
-    def __init__(self, msg, reason=None, exit_code=None):
-        super(PwnlibException, self).__init__(self, msg)
+    def __init__(self, message, reason=None, exit_code=None):
+        self.message = message
         self.reason = reason
         self.exit_code = exit_code
 
     def __repr__(self):
         s = 'PwnlibException: %s' % self.message
+
         if self.reason:
             s += '\nReason:\n'
             s += ''.join(traceback.format_exception(*self.reason))
         elif sys.exc_info()[0] not in (None, KeyboardInterrupt):
             s += '\n'
             s += traceback.format_exc()
+
         return s
